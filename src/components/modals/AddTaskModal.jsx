9a73f7780b9ecd366cef77e0f18d4777ea0d5cb5
gio mustard <mssm011009@gmail.com>
2026-03-30 09:40:20 -0700

    remove: innecesary console log

--- src/components/modals/AddTaskModal.jsx
+++ src/components/modals/AddTaskModal.jsx
@@ -34,7 +34,6 @@ export function AddTaskModal({ onAdd, onClose, open = true, initialType = 'daily
   const [isRecurring,setIsRecurring] = useState(false);
   const [isEditMode,setIsEditMode] = useState(editMode);
   const injectPayload = useCallback((payload)=>{
-    console.log(payload)
     if(payload === undefined) {
       setTitle('');
       setDescription('');
